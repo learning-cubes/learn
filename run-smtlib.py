9f6d9d8c6f81ad94e5e1e76812690e53e74c6e36
learning-cubes <82967395+learning-cubes@users.noreply.github.com>
2021-04-28 17:29:29 +0200

    Update run-smtlib.py

--- run-smtlib.py
+++ run-smtlib.py
@@ -1,49 +1,54 @@
 from maximal_cubes import *
+import mondec
 
-
-
-def run_file(smt2file, var, max_cube = True, u = False, b = False, o = True):
+def run_file(smt2file, max_cube = True, u = False, b = False, o = True, md=False):
 	target_vector = parse_smt2_file(smt2file)
-	# add lower and upper limit for the variables to make sure it is mondec
 	
-	# iterate the vector and from it into an and formula
+	# iterate the vector and from it build a AND formula
 	target = True
 	for formula in target_vector:
 		target = And(target, formula)
-		print(formula)
-	teacher = Teacher(var, target)
+
+	var = z3util.get_vars(formula)
+	print("Variables: %r" % var)
+	print("Formula: %r" % formula)
+
+	def lambda_model(value):
+		"""The mondec algorithm requires a lambda function as an input, instantiating
+		the formula with any list of free variables.
+		Note that substitue() may have an overhead (direct lambda specification
+		may be faster)
+		"""
+		return substitute(formula, *((vname, value[i]) for (i, vname) in enumerate(var)))
+	
 	start = time.time()
-	if max_cube:
-		print("test1")
+	if md:
+		result = mondec.mondec(lambda_model, var)
+	elif max_cube:
+		teacher = Teacher(var, target)
 		learner = Learner_max_cubes(var, unary = u, binary = b, optimized = o)
-		print("test2")
 		result = learner.learn(teacher)
 	else:
+		teacher = Teacher(var, target)
 		learner = Learner_trial_and_overshoot(var, unary = u, binary = b)
 		result = learner.learn(teacher,target)	
 	end = time.time()
+	print("Res: %r" % result)
 	print("Total time needed: ", end - start)
 	return result
 
 if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print("Usage: %s [overshoot-u|overshoot-b|max-u|max-b|max-o|mondec] filename.smt2")
+		sys.exit(0)
 
-	
-	print(sys.argv)
 	smt2file = sys.argv[2]
-	var_name = []
-	var = []
-	for i in range (0,len(sys.argv)):
-		if i > 2:
-			var_name.append(sys.argv[i])
-	for i in range (0,len(var_name)):
-		temp = Int(var_name[i])
-		var.append(temp)
 	
-	print(var_name, var)
 	m1 = False
 	u1 = False
 	b1 = False
 	o1 = False
+	md = False
 	if sys.argv[1] == "overshoot-u":
 		u1 = True
 	elif sys.argv[1] == "overshoot-b":
@@ -57,5 +62,9 @@ if __name__ == '__main__':
 	elif sys.argv[1] == "max-o":
 		m1 = True
 		o1 = True
-	print(m1,u1,b1,o1)
-	run_file(smt2file, var, max_cube = m1, u = u1, b = b1, o = o1)
+	elif sys.argv[1] == "mondec":
+		md= True
+	else:
+		print("Unknown solver: %s" % sys.argv[1])
+		sys.exit(1)
+	run_file(smt2file, max_cube = m1, u = u1, b = b1, o = o1, md=md)
